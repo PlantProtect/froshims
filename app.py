4eb30ffcea3dfd2977ead9e5a2aed05a030cb681
QI <965297449@qq.com>
2023-02-03 23:45:43 +0800

    add SQl but not correct

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ SPORTS = [
 def index():
   return render_template("index.html",sports = SPORTS)
 
+@app.route("/deregister",methods=["POST"])
+def deregister():
+  #Forget registrant
+  id = request.form.get("id")
+  if id:
+    db.execute("DeLETE FROM registrants WHERE id =?",id)
+  return redirect("/registrants")
 
 @app.route("/register",methods=["POST"])
 def registert():
@@ -35,12 +42,14 @@ def registert():
      return render_template("error.html",message="Invalid sport")
   
  #Remeber registerant
-  REGISTERANTS[name] =sport
+  # REGISTERANTS[name] =sport
+  db.execute("INSERT INTO registrants (name,sport) VALUES(?,?)",name,sport)
 
   #Confirm registration
   return redirect("/registrants")
 
 @app.route("/registrants")
 def registrants():
+    registrants = db.execute("SELECT * FROM registrants")
     return render_template("registrants.html",registrants=REGISTERANTS)
   
