5215b936e2659b7ad1dc3c3aeceb901355c0b075
QI <965297449@qq.com>
2023-02-06 22:54:27 +0800

    resolve deregister

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask import Flask,render_template,request,redirect
 app =Flask(__name__)
 
 db = SQL("sqlite:///froshims.db")
+# ///mean local  files
 
-REGISTERANTS = {}
+
+
 
 SPORTS = [
   "Basketball",
@@ -24,11 +26,11 @@ def deregister():
   #Forget registrant
   id = request.form.get("id")
   if id:
-    db.execute("DeLETE FROM registrants WHERE id =?",id)
+      db.execute("DELETE FROM registrants WHERE id =?",id)
   return redirect("/registrants")
 
 @app.route("/register",methods=["POST"])
-def registert():
+def register():
 
 #validate name
   name = request.form.get("name")
@@ -51,5 +53,5 @@ def registert():
 @app.route("/registrants")
 def registrants():
     registrants = db.execute("SELECT * FROM registrants")
-    return render_template("registrants.html",registrants=REGISTERANTS)
+    return render_template("registrants.html",registrants=registrants)
   
